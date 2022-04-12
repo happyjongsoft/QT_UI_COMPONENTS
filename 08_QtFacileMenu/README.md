19b975a99ba281c01768ea8618c4ddad6e58ec1d
happyjongsoft <happy.jong.soft@gmail.com>
2022-04-12 10:31:00 +0700

    QtFacileMenu

---
+++ 08_QtFacileMenu/README.md
@@ -0,0 +1,308 @@
+# Effective Qt UI Components - QtFacileMenu
+
+<img src="https://github.com/happyjongsoft/QT_UI_COMPONENTS/blob/master/08_QtFacileMenu/QtFacileMenu.gif?raw=true" width="400"/>
+
+
+## 介绍
+
+非常飘逸的 Qt 菜单控件，带有各种动画效果，用起来也十分方便。
+
+无限层级，响应键盘、鼠标单独操作，支持单快捷键。
+
+允许添加自定义 widget、layout，当做特殊的 QDialog 使用。
+
+
+
+## 简单使用
+
+1. 放入源代码
+   将 `facile_menu` 文件夹放入 Qt 程序，pro 文件的 `INCLUDEPATH` 加上对应路径，`resources` 里的资源文件 `sub_menu_arrow.png` （子菜单箭头）也导入，前缀别名为：`:/icons/sub_menu_arrow`（或按需修改）
+
+2. 包含头文件
+   `#include "facile_menu.h"`
+
+3. 创建并显示菜单
+
+   ```C++
+   // 创建菜单
+   FacileMenu* menu = new FacileMenu(this);
+   
+   // 添加动作
+   menu->addAction(QIcon(":/icons/run"), "开始播放 (&S)", [=]{
+       /* 某操作 */
+   })->tip("Ctrl+S")->disable()->hide();
+   
+   // 显示菜单
+   menu->execute(QCursor::pos());
+   ```
+
+
+
+
+## 常用操作
+
+### 连续设置
+
+```C++
+menu->addAction(QIcon(":/icons/run"), "开始播放 (&S)", [=]{})
+    ->tip("Ctrl+S")
+    ->disable(playing/*如果满足某条件(默认true)则disable，不满足跳过，下同*/)
+    ->hide(/*条件表达式*/)
+    ->uncheck(false);
+```
+
+
+
+### 子菜单
+
+```C++
+auto subMenu = menu->addMenu("子菜单2");
+
+subMenu->addAction("继续", [=]{});
+
+subMenu3->addAction("停止", [=]{})
+        ->disable(!playing);
+```
+
+
+
+### 横向菜单
+
+方式一：一口气添加
+
+```C++
+menu->addRow([=]{
+    menu->addAction("按钮1");
+    menu->addAction("按钮2");
+    menu->addAction("按钮3");
+});
+```
+
+方式二：逐个添加
+
+```C++
+menu->beginRow();
+menu->addAction(QIcon(":/icons/run"));
+menu->addAction(QIcon(":/icons/pause"));
+menu->split();
+menu->addAction(QIcon(":/icons/resume"));
+menu->addAction(QIcon(":/icons/stop"))->disable();
+menu->endRow();
+```
+
+两种方式都支持横向布局 widget
+
+
+
+### 添加 QAction
+
+支持在菜单关闭时自动 delete 传入的 action，避免内存泄漏（默认关闭）
+
+```C++
+QAction* action = ...;
+menu->addAction(action, true/*是否在菜单关闭一起时delete*/);
+```
+
+
+
+### 添加 Widget/Layout
+
+添加任意 widget 至菜单中，和菜单项并存。layout 同理。
+
+```C++
+QPushButton* button = new QPushButton("外部添加的按钮", this);
+menu->addWidget(button);
+```
+
+
+
+### 添加单选菜单
+
+> 如果要设置为checkable，请在创建时调用一次其以下任一方法：
+>
+> setCheckable(bool) / setChecked(bool) / check(bool) / uncheck(bool)
+
+```C++
+auto ac1 = subMenu2->addAction(QIcon(":/icons/run"), "带图标")->check()->linger();
+auto ac2 = subMenu2->addAction("无图标")->uncheck()->linger();
+auto ac3 = subMenu2->split()->addAction("全不选")->uncheck()->linger();
+
+// 连接点击事件
+ac1->triggered([=]{
+    subMenu2->singleCheck(ac1); // 用于单选，表示只选中ac1
+    // 这里可以用于处理其他操作
+});
+ac2->triggered([=]{
+    subMenu2->singleCheck(ac2);
+});
+ac3->triggered([=]{
+    subMenu2->uncheckAll(); // 全不选
+});
+```
+
+
+
+### 添加多选菜单
+
+```C++
+// 假装是某一个需要多选的属性
+QList<QString>* list = new QList<QString>();
+
+for (int i = 0; i < 10; i++)
+{
+    auto action = subMenu5->addAction("选项"+QString::number(i))->uncheck()->linger()->autoToggle()/*点击自动切换选中状态*/;
+    action->triggered([=]{
+        // 自己的处理流程，例如调用某个外部的方法
+        if (action->isChecked())
+            list->append(action->getText());
+        else
+            list->removeOne(action->getText());
+        qDebug() << "当前选中的有：" << *list;
+    });
+}
+```
+
+
+
+### 快速批量单选项
+
+```C++
+QStringList texts;
+for (int i = 0; i < 10; i++)
+    texts << "项目"+QString::number(i);
+static int selected = 2;
+
+menu->addOptions(texts, selected, [=](int index){
+    qDebug() << "选中了：" << (selected = index) << texts.at(index);
+});
+```
+
+
+
+### 快速批量多选项
+
+监听每一项改变的结果
+
+```C++
+// 假装是某一个需要多选的属性
+QList<int>* list = new QList<int>();
+subMenu6->addNumberedActions("选项%1", 0, 10)
+    ->setMultiCheck([=](int index, bool checked){
+        if (checked)
+            list->append(index);
+        else
+            list->removeOne(index);
+        qDebug() << "当前选中的有：" << *list;
+    });
+```
+
+
+
+### 极简批量多选项
+
+直接读取多选项结果，而不是监听多选项每一项（也可以两者结合）
+
+在`finished()`中获取`checkedItems()`，即为选中项
+
+```C++
+QList<QString>* list = new QList<QString>();
+subMenu7->addNumberedActions("选项%1", 0, 15)->setMultiCheck()
+    ->finished([=]{
+        *list = subMenu7->checkedItemTexts();
+        qDebug() << "最终选中：" << *list;
+    });
+```
+
+
+
+### 菜单项 API
+
+`addAction()`后，可直接设置菜单项的一些属性，包括以下：
+
+> 第一个参数为`bool`类型的，表示**满足此条件才修改设置**，例如：
+>
+> ```C++
+> bool needHide = false;
+> action->hide(needHide); // 不满足隐藏条件，即无视此语句
+> ```
+
+```C++
+// 菜单项右边快捷键区域的文字
+// 如果要使用，建议用：setTipArea 来额外添加设置右边空白宽度
+FacileMenuItem* tip(QString sc);
+FacileMenuItem* tip(bool exp, QString sc);
+
+// 鼠标悬浮提示
+FacileMenuItem* tooltip(QString tt);
+FacileMenuItem* tooltip(bool exp, QString tt);
+
+// 触发（单击、回车键）后，参数为 Lambda 表达式
+FacileMenuItem* triggered(FuncType func);
+FacileMenuItem* triggered(bool exp, FuncType func);
+
+// 当参数表达式为true时生效，false时忽略，下同
+FacileMenuItem* disable(bool exp = true);
+FacileMenuItem* enable(bool exp = true);
+
+FacileMenuItem* hide(bool exp = true);
+FacileMenuItem* visible(bool exp = true);
+
+FacileMenuItem* check(bool exp = true);
+FacileMenuItem* uncheck(bool exp = true);
+FacileMenuItem* toggle(bool exp = true);
+
+// 设置data，一般用于单选、多选
+FacileMenuItem* setData(QVariant data);
+QVariant getData();
+
+FacileMenuItem* text(bool exp, QString str);
+// 当表达式为true时，设置为tru文字，否则设置为fal文字
+FacileMenuItem* text(bool exp, QString tru, QString fal);
+
+FacileMenuItem* fgColor(QColor color);
+FacileMenuItem* fgColor(bool exp, QColor color);
+
+FacileMenuItem* bgColor(QColor color);
+FacileMenuItem* bgColor(bool exp, QColor color);
+
+// 插入前缀
+FacileMenuItem* prefix(bool exp, QString pfix);
+FacileMenuItem* prefix(QString pfix);
+
+// 插入后缀，参数3支持类似 "action后缀 (K)" 这样的格式
+FacileMenuItem* suffix(bool exp, QString sfix, bool inLeftParenthesis = true);
+FacileMenuItem* suffix(QString sfix, bool inLeftParenthesis = true);
+
+FacileMenuItem* icon(bool ic, QIcon icon);
+
+// 设置边界：半径、颜色
+FacileMenuItem* borderR(int radius = 3, QColor co = Qt::transparent);
+
+// 点击后是否保持菜单显示（默认点一下就隐藏菜单）
+FacileMenuItem* linger();
+
+// 满足 exp 时执行 trueLambda 表达式，否则执行 falseLambda 表达式
+FacileMenuItem* ifer(bool exp, trueLambda, falseLambda = nullptr);
+
+// 逻辑控制
+FacileMenuItem* ifer(bool exp); // 满足条件时才继续，下同
+FacileMenuItem* elifer(bool exp);
+FacileMenuItem* elser();
+
+FacileMenuItem* switcher(int value);
+FacileMenuItem* caser(int value, matchedLambda); // 匹配时执行lambda，无需break
+FacileMenuItem* caser(int value); // 结束记得breaker（允许忘掉~）
+FacileMenuItem* breaker();
+FacileMenuItem* defaulter();
+
+// 取消后面所有命令（无视层级，相当于函数中return）
+FacileMenuItem* exiter(bool ex = true);
+```
+
+
+> 注意：由于加了一些容错处理（例如caser可以不用写breaker），无法进行if/switch的多层嵌套（较多的逻辑运算不建议放在菜单中）
+
+
+
+[happy.jong.soft@gmail.com](mailto:happy.jong.soft@gmail.com) \
+[YouTube](https://www.youtube.com/channel/UCzcpR2jPKBYXvKFp6kBMdGA)
---
+++ 08_QtFacileMenu/mainwindow.cpp
@@ -0,0 +1,373 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    static bool faster_checked = true;
+    static bool slower_checked = false;
+
+    FacileMenu* menu = (new FacileMenu(this))
+        ->setTipArea("Ctrl+P")
+        ->setSplitInRow(true);
+
+    menu->addAction(QIcon(":/icons/run"), "开始播放 (&S)", [=]{
+        qDebug() << "=>开始播放";
+    })->tip("Ctrl+S");
+
+    menu->addAction(QIcon(":/icons/pause"), "暂停 (&P)", [=]{
+        qDebug() << "=>暂停";
+    })->tip("Ctrl+P")->suffix(true, "☆");
+
+    menu->addAction(QIcon(":/icons/resume"), "继续 (&R)", [=]{
+        qDebug() << "=>继续";
+    })->disable()->tip("Ctrl+R");
+
+    menu->addAction(QIcon(":/icons/stop"), "停止 (&T)", [=]{
+        qDebug() << "=>停止";
+    })->tip("Ctrl+T");
+    menu->addSeparator();
+
+    menu->addAction(QIcon(":/icons/faster"), "加速", [=]{
+        qDebug() << "=>加速";
+        faster_checked = !faster_checked;
+    })->check(faster_checked)->linger()
+            ->ifer(false)
+            ->prefix("1")
+            ->elser()
+            ->prefix("2");
+
+    int number = 3;
+    menu->addAction(QIcon(":/icons/slower"), "减速", [=]{
+        qDebug() << "=>减速";
+        slower_checked = !slower_checked;
+    })->check(slower_checked)->linger()
+            ->switcher(number)
+            ->caser(1)
+            ->suffix("1")
+            ->breaker()
+            ->caser(2)
+            ->suffix("2")
+            // ->breaker()
+            ->caser(3)
+            ->suffix(" caser")
+            ->breaker()
+            ->caser(4)
+            ->suffix("4")
+            ->breaker()
+            ->caser(6, [=]{ qDebug() << "(^_−)☆"; })
+            ->defaulter()
+            ->suffix("5")
+            ->breaker();
+
+    menu->addRow([=]{
+        menu->addAction("按钮1");
+        menu->addAction("按钮2")->borderR();
+        menu->addAction("按钮3");
+    });
+
+    auto subMenu = menu->addMenu("子菜单0");
+    {
+        subMenu->addAction(QIcon(":/icons/run"), "开始播放", [=]{
+            qDebug() << "=>开始播放";
+        });
+
+        subMenu->addAction(QIcon(":/icons/pause"), "暂停", [=]{
+            qDebug() << "=>暂停";
+        });
+
+        subMenu->addAction(QIcon(":/icons/resume"), "继续", [=]{
+            qDebug() << "=>继续";
+        })->disable();
+
+        subMenu->addAction(QIcon(":/icons/stop"), "停止", [=]{
+            qDebug() << "=>停止";
+        });
+
+        subMenu->addAction(QIcon(":/icons/faster"), "加速", [=]{
+            qDebug() << "=>加速";
+            faster_checked = !faster_checked;
+        })->setChecked(faster_checked);
+
+        auto subMenu2 = subMenu->addMenu("子菜单");
+        {
+            subMenu2->addAction(QIcon(":/icons/run"), "开始播放", [=]{
+                qDebug() << "=>开始播放";
+            });
+
+            subMenu2->addAction(QIcon(":/icons/pause"), "暂停", [=]{
+                qDebug() << "=>暂停";
+            });
+
+            subMenu2->addAction(QIcon(":/icons/resume"), "继续", [=]{
+                qDebug() << "=>继续";
+            })->disable();
+
+            subMenu2->addAction(QIcon(":/icons/stop"), "停止", [=]{
+                qDebug() << "=>停止";
+            });
+
+            subMenu2->addAction(QIcon(":/icons/faster"), "加速", [=]{
+                qDebug() << "=>加速";
+                faster_checked = !faster_checked;
+            })->setChecked(faster_checked);
+
+            auto subMenu3 = subMenu2->addMenu("子菜单");
+            subMenu3->addAction("没有子菜单了");
+        }
+    }
+
+    auto subMenu2 = menu->addMenu("单选菜单");
+    {
+        auto ac1 = subMenu2->addAction(QIcon(":/icons/run"), "带图标")->check()->linger();
+        auto ac2 = subMenu2->addAction("无图标")->uncheck()->linger();
+        auto ac3 = subMenu2->split()->addAction("全不选")->uncheck()->linger();
+
+        // 连接点击事件
+        ac1->triggered([=]{
+            subMenu2->singleCheck(ac1); // 用于单选，表示只选中ac1
+            // 这里可以用于处理其他操作
+        });
+        ac2->triggered([=]{
+            subMenu2->singleCheck(ac2);
+        });
+        ac3->triggered([=]{
+            subMenu2->uncheckAll(); // 全不选
+        });
+    }
+
+    auto subMenu4 = menu->addMenu("快速批量单选项");
+    {
+        QStringList texts;
+        QList<int> values;
+        for (int i = 0; i < 10; i++)
+        {
+            int val = qrand() % 1000;
+            texts << "项目"+QString::number(val);
+            values << val;
+        }
+        static int selected = 2;
+
+        subMenu4->addOptions(texts, selected, [=](int index){
+            qDebug() << "选中了：" << (selected = index) << "，值：" << values.at(index);
+        });
+        // 这里不建议（也没有必要）修改checked状态，因为点了就隐藏掉了
+    }
+
+    auto subMenu5 = menu->addMenu("多选菜单");
+    {
+        // 假装是某一个需要多选的属性
+        QList<int>* list = new QList<int>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            int val = qrand() % 1000; // 放入菜单项的自定义数据
+            auto action = subMenu5->addAction("选项"+QString::number(val))->setData(val)->uncheck()->autoToggle()->linger();
+            action->triggered([=]{
+                // 自己的处理流程，例如调用某个外部的方法
+                if (action->isChecked())
+                    list->append(action->getData().toInt());
+                else
+                    list->removeOne(action->getData().toInt());
+                qDebug() << "当前选中的有：" << *list;
+            });
+        }
+
+        subMenu5->finished([=]{
+            if (list->size())
+                qDebug() << "多选 最终选中：" << *list;
+            delete list;
+        });
+    }
+
+    auto subMenu6 = menu->addMenu("快速批量多选项");
+    {
+        // 假装是某一个需要多选的属性
+        QList<int>* list = new QList<int>();
+        subMenu6->addNumberedActions("选项%1", 0, 10)
+            ->setMultiCheck([=](int index, bool checked){
+                if (checked)
+                    list->append(index);
+                else
+                    list->removeOne(index);
+                qDebug() << "当前选中的有：" << *list;
+            });
+    }
+
+    auto subMenu7 = menu->addMenu("极简批量多选项");
+    {
+        QList<QString>* list = new QList<QString>();
+        subMenu7->addNumberedActions("选项%1", 0, 15)->setMultiCheck()
+                ->finished([=]{
+            *list = subMenu7->checkedItemTexts();
+            if (list->size())
+                qDebug() << "极简多选项 最终选中：" << *list;
+        });
+    }
+
+    auto subMenu8 = menu->addMenu("批量数字项");
+    {
+        subMenu8->addNumberedActions("选项%1", 3, 13, [&](FacileMenuItem* item, int i){
+            item->setChecked(i%5==0)->linger()->autoToggle();
+        })->finished([=]{
+            auto list = subMenu8->checkedItemTexts();
+            if (list.size() != 2)
+                qDebug() << "批量数字项 最终选中：" << list;
+        });
+    }
+
+    auto subMenu3 = menu->addMenu("判断语句");
+    {
+        static bool b = false;
+        subMenu3->addAction("if else 判断")
+                ->tooltip("试一下 ifer elser")
+                ->text(b, "当前：if", "当前：else")
+                ->triggered([=]{b = !b;})
+                ->ifer(b)
+                ->check()
+                ->elser()
+                ->uncheck();
+    }
+
+    // 导入QMenu和QAction
+    {
+        QMenu* m = new QMenu("QMenu菜单", this);
+        QAction* action1 = new QAction("子action1", this);
+        QAction* action2 = new QAction("子action2", this);
+        QAction* action3 = new QAction("子action3", this);
+        m->addAction(action1);
+        m->addAction(action2);
+        m->addAction(action3);
+        QMenu* m2 = new QMenu("QMenu菜单2", this);
+        QAction* action21 = new QAction("子action21", this);
+        QAction* action22 = new QAction("子action22", this);
+        QAction* action23 = new QAction("子action23", this);
+        m2->addAction(action21);
+        m2->addAction(action22);
+        m2->addAction(action23);
+        action2->setMenu(m2);
+        connect(action1, &QAction::triggered, this, [=]{ qDebug() << "action1.triggered"; });
+        connect(action21, &QAction::triggered, this, [=]{ qDebug() << "action21.triggered"; });
+
+        menu->addMenu(m);
+    }
+
+    menu->exec(QCursor::pos());
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    static bool faster_checked = true;
+    static bool slower_checked = false;
+
+    FacileMenu* menu = new FacileMenu(this);
+    menu->setTipArea("Ctrl+P");
+
+    menu->addRow([=]{
+        menu->addAction("按钮1")->disable();
+        menu->addAction("按钮2")->disable();
+        menu->addAction("按钮3")->text(true, "按钮3 (&K)");
+    });
+
+    menu->addAction(QIcon(":/icons/run"), "开始播放 (&S)", [=]{
+        qDebug() << "=>开始播放";
+    })->tip("Ctrl+S")->fgColor(Qt::blue);
+
+    menu->addAction(QIcon(":/icons/pause"), "暂停 (&P)", [=]{
+        qDebug() << "=>暂停";
+    })->tip("Ctrl+P")->bgColor(true, Qt::gray);
+
+    QPushButton* button = new QPushButton("外部添加的按钮", this);
+    menu->addWidget(button);
+
+    QHBoxLayout* btn_hlayout = new QHBoxLayout;
+    QPushButton* btn1= new QPushButton("按钮1", this);
+    QPushButton* btn2 = new QPushButton("按钮2", this);
+    btn_hlayout->addSpacing(40);
+    btn_hlayout->addWidget(btn1);
+    btn_hlayout->addItem(new QSpacerItem(4, 0));
+    btn_hlayout->addWidget(btn2);
+    menu->addLayout(btn_hlayout);
+
+    menu->addAction(QIcon(":/icons/resume"), "继续 (&R)", [=]{
+        qDebug() << "=>继续";
+    })->disable()->tip("Ctrl+R");
+
+    menu->addAction(QIcon(":/icons/stop"), "停止 (&T)", [=]{
+        qDebug() << "=>停止";
+    })->tip("Ctrl+T");
+    menu->addSeparator();
+
+    menu->addAction(QIcon(), "静态回调方法", staticFunction);
+//    menu->fun2(1, "类内回调方法", this, &MainWindow::classFunction);
+
+    menu->addAction(QIcon(":/icons/faster"), "加速", [=]{
+        qDebug() << "=>加速";
+        faster_checked = !faster_checked;
+    })->setChecked(faster_checked);
+
+    menu->addAction(QIcon(":/icons/slower"), "减速", [=]{
+        qDebug() << "=>减速";
+        slower_checked = !slower_checked;
+    })->setChecked(slower_checked);
+
+    menu->beginRow();
+    menu->addAction(QIcon(":/icons/run"));
+    menu->addAction(QIcon(":/icons/pause"));
+    menu->split()->addAction(QIcon(":/icons/resume"));
+    menu->addAction(QIcon(":/icons/stop"))->disable();
+    menu->endRow();
+
+    menu->addAction(QIcon(":/icons/faster"), "加速", [=]{
+        qDebug() << "=>加速";
+        faster_checked = !faster_checked;
+    })->setChecked(faster_checked)->disable();
+
+    menu->addAction(QIcon(":/icons/slower"), "减速", [=]{
+        qDebug() << "=>减速";
+        slower_checked = !slower_checked;
+    })->setChecked(slower_checked)->disable();
+
+    menu->beginRow();
+    menu->addAction("按钮1");
+    menu->addAction("按钮2")->disable();
+    menu->addAction("按钮3")->disable();
+    menu->endRow();
+
+    menu->exec(QRect(mapToGlobal(ui->pushButton_2->pos()), ui->pushButton_2->size()), true, QCursor::pos());
+}
+
+void MainWindow::staticFunction()
+{
+    qDebug() << "静态方法回调";
+}
+
+void MainWindow::classFunction()
+{
+    qDebug() << "类内方法回调";
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+
+    FacileMenu* menu = (new FacileMenu(this));
+
+    for (int i = 0; i < 80; i++)
+    {
+        menu->addAction("这是一个菜单" + QString::number(i));
+    }
+
+    menu->exec(QCursor::pos());
+}
